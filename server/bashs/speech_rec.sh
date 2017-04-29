85cc44221b0bed7e24f9ff14b9cd49d961d74ec5
KehxD <sharru.moeller@gmail.com>
2017-04-29 14:56:52 +0200

    bash another update

--- server/bashs/speech_rec.sh
+++ server/bashs/speech_rec.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 source /opt/ros/kinetic/setup.bash
-Roboy_IP="10.183.113.58"
+ROBOY_IP="10.183.113.58"
 
 export ROS_IP=$ROBOY_IP # make sure you are on the same network as Roboy
 export ROS_HOSTNAME=$ROBOY_IP
-export ROS_MASTER_URI=http://$Roboy_IP:11311
+export ROS_MASTER_URI=http://$ROBOY_IP:11311
 
 rosservice call /TextSpoken "{}"
