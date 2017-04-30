17c643d7efaa61f9872040d4e67ca4e8953f6a63
KehxD <sharru.moeller@gmail.com>
2017-04-30 21:20:22 +0200

    HTC Vive listener without server send()

---
+++ server/htc_tracker.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import sys
+import rospy
+from geometry_msgs.msg import PoseWithCovarianceStamped
+
+global quest
+global prevent_repeat
+
+def callback(data):
+	fun = data.pose.pose.position;
+	if quest=="jump":
+		if fun.y>0: #to change
+			#notify server
+			prevent_repeat = True;
+		else:
+			prevent_repeat = False;
+    
+def listener():
+
+    rospy.init_node('listener', anonymous=True)
+
+    rospy.Subscriber("initialpose", PoseWithCovarianceStamped, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+	quest = sys.argv[1]
+	prevent_repeat = False
+	listener()
