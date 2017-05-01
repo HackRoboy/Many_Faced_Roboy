5fa0fd831d7fa7ecd52f0f4ff48452a7f8a0ba88
KehxD <sharru.moeller@gmail.com>
2017-05-01 11:17:30 +0200

    test update and some changes from original repos

--- server/htc_tracker.py
+++ server/htc_tracker.py
@@ -5,16 +5,18 @@ from geometry_msgs.msg import PoseWithCovarianceStamped
 
 global quest
 global prevent_repeat
+global jumped
 
 def callback(data):
 	fun = data.pose.pose.position;
 	print(fun.x, fun.y, fun.z)
 	if quest=="jump":
-		if fun.y>0: #to change
-			#notify server
-			prevent_repeat = True;
+		if fun.y>1: #to change
+			print "Quest done"
+			prevent_repeat = True
+			jump=True
 		else:
-			prevent_repeat = False;
+			prevent_repeat = False
     
 def listener():
 
@@ -25,5 +27,6 @@ def listener():
 
 if __name__ == '__main__':
 	quest = sys.argv[1]
+	jump = False
 	prevent_repeat = False
 	listener()
